e37b5d1fb17efd53333a5a41163b427464e70766
Patrick Daly <pmdaly12@gmail.com>
2022-04-04 11:40:24 -0700

    updated live contract address, fixed IStrat & we passin

--- src/interfaces/IStrategy.sol
+++ src/interfaces/IStrategy.sol
@@ -4,4 +4,10 @@ pragma solidity 0.8.12;
 
 interface IStrategy {
     function estimatedTotalAssets() external view returns (uint256);
+
+    function vault() external view returns (address);
+
+    function setEmergencyExit() external;
+
+    function harvest() external;
 }
--- src/test/StrategyMigration.t.sol
+++ src/test/StrategyMigration.t.sol
@@ -4,7 +4,9 @@ pragma solidity 0.8.12;
 import {StrategyFixture} from "./utils/StrategyFixture.sol";
 import {Strategy} from "../Strategy.sol";
 import {console} from "./utils/console.sol";
+
 import {IStrategy} from "../interfaces/IStrategy.sol";
+import {IVault} from "../interfaces/Vault.sol";
 
 contract StrategyMigrationTest is StrategyFixture {
     function setUp() public override {
@@ -28,7 +30,6 @@ contract StrategyMigrationTest is StrategyFixture {
         skip(1);
         vm_std_cheats.prank(gov);
         strategy.harvest();
-        console.log("finished harvesting");
         assertRelApproxEq(strategy.estimatedTotalAssets(), _amount, DELTA);
 
         // migrate to a new strategy
@@ -37,5 +38,29 @@ contract StrategyMigrationTest is StrategyFixture {
         vm_std_cheats.prank(gov);
         vault.migrateStrategy(address(strategy), address(strategyNew));
         assertRelApproxEq(IStrategy(strategyNew).estimatedTotalAssets(), _amount, DELTA);
+    }
+    
+    function testRealMigration() public {
+        address liveStrat = 0xC7af91cdDDfC7c782671eFb640A4E4C4FB6352B4;
+        address liveVault = IStrategy(liveStrat).vault();
+
+        // deploy a new strategy with this vault
+        vm_std_cheats.prank(strategist);
+        address fixedStrategy = deployStrategy(address(liveVault));
+
+        // migrate the live strat to the new (fixed) strat 
+        vm_std_cheats.prank(gov);
+        IVault(liveVault).migrateStrategy(liveStrat, fixedStrategy);
+        assertGe(
+            IStrategy(fixedStrategy).estimatedTotalAssets(),
+            IVault(liveVault).strategies(fixedStrategy).totalDebt
+        );
+
+        // emergency exit and check harvest
+        vm_std_cheats.prank(strategist);
+        IStrategy(fixedStrategy).setEmergencyExit();
+        vm_std_cheats.prank(strategist);
+        IStrategy(fixedStrategy).harvest();
+        assertEq(IStrategy(fixedStrategy).estimatedTotalAssets(), 0);
     }
 }
